2de20b27be3b5383df336158b439e879635911fd
Flynn Duniho <flynn@harveyosity.org>
2024-01-25 21:07:43 +0000

    try to get user location

--- js/compass.js
+++ js/compass.js
@@ -1,28 +1,43 @@
 // From https://dev.to/orkhanjafarovr/real-compass-on-mobile-browsers-with-javascript-3emi
+var debug;
 var arrow;
+var position;
+var target = [51.521900, -0.124490];
 const isIOS = /iPad|iPhone|iPod|Apple/.test(navigator.userAgent);
-if (isIOS) {
-	alert("Hi! You're on iOS!");
-}
 
 function startCompass() {
 	arrow.style.display = "block";
+
 	if (isIOS) {
 		DeviceOrientationEvent.requestPermission()
 			.then((response) => {
 				if (response === "granted") {
-					window.addEventListener("deviceorientation", handler, true);
+					window.addEventListener("deviceorientation", compassHandler, true);
 				} else {
 					alert("has to be allowed!");
 				}
 			})
 			.catch(() => alert("not supported"));
 	} else {
-		window.addEventListener("deviceorientationabsolute", handler, true);
+		window.addEventListener("deviceorientationabsolute", compassHandler, true);
+	}
+	navigator.geolocation.getCurrentPosition(locationHandler, locationError);
+}
+
+var f;
+function locationError(e) {
+	if (!f) {
+		alert(e.message);
+		f = 1;
 	}
 }
 
-function handler(e) {
+function locationHandler(e) {
+	position = [e.coords.longitude, e.coords.latitude];
+	debug.innerHTML = position;
+}
+
+function compassHandler(e) {
 	let alpha;
 	// Check for iOS property
 	if (e.webkitCompassHeading) {
@@ -35,11 +50,15 @@ function handler(e) {
 		}
 	}
 
+	if (!position) {
+		alpha = Math.random() * 360;
+	}
 	arrow.style.transform = 'rotate(' + alpha + 'deg)';
 }
 
 window.onload = function() {
 	arrow = document.getElementsByClassName("arrow")[0];
+	debug = document.getElementById("debug");
 	arrow.style.display = "none";
 }
 
